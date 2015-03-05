e87ddda571c74b8042031a5d382e2168400b35f3
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-03-05 16:48:03 +0530

    Modified typos for map method call

--- help_the_needy/help_the_needy/views.py
+++ help_the_needy/help_the_needy/views.py
@@ -4,4 +4,4 @@ def map(request):
     loc = ''
     if request.method == 'PUT':
         loc = request.PUT.get('location', '')
-    return render_to_response("map.html", {'location': loc})
+    return render_to_response("display_map.html", {'location': loc})
