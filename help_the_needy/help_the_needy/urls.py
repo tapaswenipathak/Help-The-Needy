b4e44c343a1d93ce4a7bfdb147bc5cc996529043
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-03-07 02:26:24 +0530

    fixed broken url from '2' to map2

--- help_the_needy/help_the_needy/urls.py
+++ help_the_needy/help_the_needy/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'help_the_needy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^map', 'help_the_needy.views.map'),
-    url(r'^map2', 'help_the_needy.views.2'),
+    #url(r'^map', 'help_the_needy.views.map'),
+    url(r'^map2', 'help_the_needy.views.map2'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
