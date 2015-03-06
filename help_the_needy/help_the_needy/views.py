a9914a1a4e2f464a14945909e6204ed4b921eea8
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-03-06 15:54:24 +0530

    home view modified to redirect to index.

--- help_the_needy/help_the_needy/views.py
+++ help_the_needy/help_the_needy/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def home(request):
-	return render_to_response('my-modified.html')
+	return render_to_response('index.html')
 
 def map(request):
     loc = ''
