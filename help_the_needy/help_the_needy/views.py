d1cb4155da5f18a7d85329fb12b71aeadfeea9d7
Tapasweni Pathak <tapaswenipathak@gmail.com>
2015-03-06 12:21:26 +0530

    show place name in h1

--- help_the_needy/help_the_needy/views.py
+++ help_the_needy/help_the_needy/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def map(request):
     loc = ''
-    if request.method == 'PUT':
-        loc = request.PUT.get('location', '')
-    return render_to_response("map.html", {'location': loc})
+    if request.method == 'POST':
+        loc = request.POST.get('location', '')
+    return render_to_response('display_map.html', {'location': loc}, context_instance=RequestContext(request))
