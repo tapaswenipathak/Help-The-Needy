9ff4f2d0b76f52c7d20ef81641ef4bb162c0b5c6
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-03-07 22:10:32 +0530

    Commit in between to share code :P

--- help_the_needy/help_the_needy/views.py
+++ help_the_needy/help_the_needy/views.py
@@ -2,29 +2,33 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from googleplaces import GooglePlaces
 
-YOUR_API_KEY = 'AIzaSyBQB2yST-musLAYmW7o7d3XSM9zPEjE_aY'
+YOUR_API_KEY = 'AIzaSyBxyEeoSD4AiuaAOW5MWJgUwEJkdHEN6xs'
 
 google_places = GooglePlaces(YOUR_API_KEY)
 
 def home(request):
-	return render_to_response('index.html')
+	return render_to_response('index.html', context_instance=RequestContext(request))
 
 def map(request):
+	print("map is called \o/")
 	ngos_name = []
+	ngos_address = {}
 	loc = ""
 	if request.method == 'POST':
+		print("with post method!")
 		loc = request.POST.get('location', '')
-		print loc
+		print("Inside map ", loc)
 	query_result = google_places.nearby_search(
-			location="Dilshad Garden", keyword='ngos and non profit organizations',
+			location=loc, keyword='ngos and non profit organizations',
 			radius=20000)
+	print("heeeeeeeeeeeeeeeeeeeeeeeeeeeere is query_result", query_result.places)
 	for place in query_result.places:
 		#place.get_details()
-		print place.name
+		print place.name, place.geo_location, place.reference
 		#print place.formatted_address
 		ngos_name.append(place.name)
 		#print place.geo_location
-	return render_to_response('display_list.html', {'location': ngos_name}, context_instance=RequestContext(request))
+	return render_to_response('display_map.html', {'location': ngos_name}, context_instance=RequestContext(request))
 
 def map2(request):
 	loc = request.POST.get('location', '')
