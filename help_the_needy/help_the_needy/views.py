5d3c9b20369da735bb3fe40edac2f6a839a46c5d
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-03-08 00:06:44 +0530

    Map showing static NGO's data but this is not mobile responsive.

--- help_the_needy/help_the_needy/views.py
+++ help_the_needy/help_the_needy/views.py
@@ -11,24 +11,23 @@ def home(request):
 
 def map(request):
 	print("map is called \o/")
-	ngos_name = []
+	ngos_name = {'NGO1':'abcbcbc', 'NGO2':'uuuuuu'}
 	ngos_address = {}
 	loc = ""
 	if request.method == 'POST':
-		print("with post method!")
 		loc = request.POST.get('location', '')
 		print("Inside map ", loc)
-	query_result = google_places.nearby_search(
-			location=loc, keyword='ngos and non profit organizations',
-			radius=20000)
-	print("heeeeeeeeeeeeeeeeeeeeeeeeeeeere is query_result", query_result.places)
-	for place in query_result.places:
-		#place.get_details()
-		print place.name, place.geo_location, place.reference
-		#print place.formatted_address
-		ngos_name.append(place.name)
-		#print place.geo_location
-	return render_to_response('display_map.html', {'location': ngos_name}, context_instance=RequestContext(request))
+	# query_result = google_places.nearby_search(
+	# 		location=loc, keyword='ngos and non profit organizations',
+	# 		radius=20000)
+	# print("heeeeeeeeeeeeeeeeeeeeeeeeeeeere is query_result", query_result.places)
+	# for place in query_result.places:
+	# 	#place.get_details()
+	# 	print place.name, place.geo_location, place.reference
+	# 	#print place.formatted_address
+	# 	ngos_name.append(place.name)
+	# 	#print place.geo_location
+	return render_to_response('display_map.html', {'location': loc, 'ngos_name':ngos_name}, context_instance=RequestContext(request))
 
 def map2(request):
 	loc = request.POST.get('location', '')
