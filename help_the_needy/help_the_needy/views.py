f66b821bb5f434a894b475b1e31c935ff5d50ce8
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-03-08 16:43:13 +0530

    Correct NGO names are coming on display_list2.html :)

--- help_the_needy/help_the_needy/views.py
+++ help_the_needy/help_the_needy/views.py
@@ -13,19 +13,20 @@ def map(request):
 	ngos_name = {'NGO1':'abcbcbc', 'NGO2':'uuuuuu', 'NGO3':"thierd one"}
 	ngos = {}
 	loc = ""
-	# if request.method == 'POST':
-	# 	loc = request.POST.get('location', '')
-	# 	print("Inside map ", loc)
-	# query_result = google_places.nearby_search(
-	# 		location=loc, keyword='ngos and non profit organizations',
-	# 		radius=20000)
-	# print(query_result.places)
-	# for place in query_result.places:
-	# 	print(place.name, place.formatted_address, "@@@@@@@@@@@2")
-	# 	ngos[place.name] = place.formatted_address
-	# print("Here is our dict")
-	# print(ngos)
-	return render_to_response('display_list2.html', {'location': 'Indore, India', 'ngos_name':ngos_name}, context_instance=RequestContext(request))
+	if request.method == 'POST':
+		loc = request.POST.get('location', '')
+		print("Inside map ", loc)
+	query_result = google_places.nearby_search(
+			location=loc, keyword='ngos and non profit organizations',
+			radius=20000)
+	print("query_result is gnerated! length  of places = ", len(query_result.places))
+	for place in query_result.places:
+		place.get_details()
+		print(place.name, place.formatted_address, "@@@@@@@@@@@")
+		ngos[place.name] = place.formatted_address
+	print("Here is our dict")
+	print(ngos)
+	return render_to_response('display_list2.html', {'location': 'Indore, India', 'ngos':ngos}, context_instance=RequestContext(request))
 
 def map2(request):
 	loc = request.POST.get('location', '')
